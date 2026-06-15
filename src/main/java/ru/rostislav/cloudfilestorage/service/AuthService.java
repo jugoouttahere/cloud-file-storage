543f4b27a6d798c348c619473891fd003c80290e
jugoouttahere <gangnamstylelive123@gmail.com>
2026-06-15 19:52:32 +0300

    add AuthService with basic signatures

---
+++ src/main/java/ru/rostislav/cloudfilestorage/service/AuthService.java
@@ -0,0 +1,21 @@
+package ru.rostislav.cloudfilestorage.service;
+
+import org.springframework.stereotype.Service;
+import ru.rostislav.cloudfilestorage.dto.auth.UserRequest;
+import ru.rostislav.cloudfilestorage.dto.auth.UserResponse;
+
+@Service
+public class AuthService {
+
+    public UserResponse register(UserRequest request) {
+        return null;
+    }
+
+    public UserResponse login(UserRequest request) {
+        return null;
+    }
+
+    public void logout(UserRequest request) {
+
+    }
+}
