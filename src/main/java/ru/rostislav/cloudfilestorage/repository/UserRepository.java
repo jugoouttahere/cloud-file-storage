cf146329e1ca6e1d82e47a6f19bd9d86093902c7
jugoouttahere <gangnamstylelive123@gmail.com>
2026-06-15 15:57:49 +0300

    add UserRepository

---
+++ src/main/java/ru/rostislav/cloudfilestorage/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.rostislav.cloudfilestorage.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.rostislav.cloudfilestorage.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+
+    boolean existsByLogin(String login);
+
+}
